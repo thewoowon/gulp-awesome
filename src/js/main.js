ccdec8f970dbf42cd2862908c6b09acbb94cdeb3
thewoowon <thewoowon@naver.com>
2022-09-25 19:31:00 +0900

    2022/09/25 feat : add bro and gh-gulp

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,4 @@ import { random } from "./util";
 const randomNum1 = random(10);
 const randomNum2 = random(20);
 
-console.log(`one - ${randomNum}, two - ${randomNum2}`);
+console.log(`one - ${randomNum1}, two - ${randomNum2}`);
